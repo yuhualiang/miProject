c385f555d8adfb457b835a16318a741a801bcfcb
yuhualiang <yuhualiang@tidemedia.com>
2020-09-20 01:11:48 +0800

    列表页样式编写

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import a from './a';
-// import './a.css';
 
-// let a : string = 'hello';
-console.log(a);
+import './main.css';
+
+let listItem = document.querySelectorAll('#list li');
+
+for (let i = 0; i < listItem.length; i++) {
+  listItem[i].addEventListener('click', function() {
+
+    let url = this.dataset.url;
+    let title = this.dataset.title;
+
+    console.log(url, title);
+  })
+}
